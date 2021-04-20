47389168bc61d207ab6da1526a5135b8f991c33e
Sean Lyons <seanlyons@Seans-MacBook-Pro.local>
2021-04-19 23:22:26 -0400

    starting with auth flow

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import logo from './spotify_logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import React, { useState } from 'react';
+
+
 
 function App() {
+
+  const [signin, setSignin] = useState(false);
+
+
+
   return (
+  
+  <Router>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Spotify Taste Analyzer
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <ul>
+            <li>
+              <Link className="home-link" to="/">home</Link>
+            </li>
+            <li> {
+              signin ? (    <Link className="account-link" to="/account">sign out</Link>
+              ) : (<a href="">Sign In</a>)
+              }
+
+            </li>
+          </ul>
+        </nav>
       </header>
-    </div>
+
+    <Switch>
+      <Route path="/">        
+        <Home />
+      </Route>
+      <Route path="/acount">        
+        <Account />
+      </Route>
+    </Switch>
+  </div>
+  </Router>
+  
   );
 }
 
+function Home() {
+  return (
+    <div className="home">
+  <h2>Home</h2>
+
+    </div>
+  );
+}
+function Account() {
+  return (
+    <div className="home">
+  <h2>Your Account</h2>
+
+    </div>
+  );
+}
 export default App;
